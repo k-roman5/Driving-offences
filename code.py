6f1cda7a9b490563fc8e89b6b02b4066cfe303f4
AidenC16 <116443447+AidenC16@users.noreply.github.com>
2022-10-22 15:12:50 +0100

    Update code.py
    change column names

--- code.py
+++ code.py
@@ -4,4 +4,9 @@ import pandas as pd
 
 data = 'traffic_violaions.csv'
 df = pd.read_csv(data, header=None)
-df.head()
+#print(df.head())
+
+col_names = ['date','time','cname','gender','raw_age','age','race','raw_violation','violation','search','type','outcome','arrested','duration','drugs']
+
+df.columns = col_names
+print(df.columns)

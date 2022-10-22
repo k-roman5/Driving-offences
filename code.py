97367809b5a49e6604fa574aa1a49340e8eda901
romank2 <102798439+k-roman5@users.noreply.github.com>
2022-10-22 16:15:36 +0100

    Multinomial Naïve Bayes classification updated

--- code.py
+++ code.py
@@ -1,26 +1,39 @@
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB
+
+#from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import MultinomialNB
+
+
 import numpy as np
 import pandas as pd
 
 
 data = 'traffic_violaions.csv'
 df = pd.read_csv(data, header=None)
-#df.head()
+# df.head()
 
-col_names = ['date','time','cname','gender','raw_age','age','race','raw_violation','violation','search','type','outcome','arrested','duration','drugs']
+
+# Configuration dataset
+col_names = ['date', 'time', 'cname', 'gender', 'raw_age', 'age', 'race', 'raw_violation',
+             'violation', 'search', 'type', 'outcome', 'arrested', 'duration', 'drugs']
 
 df.columns = col_names
 print(df.columns)
 
 categorical = [var for var in df.columns if df[var].dtype == 'O']
 print(df[categorical].isnull().sum())
+
 print(df.dropna(thresh=2))
 
+
 X = df.drop(['arrested'], axis=1)
+Y = df['arrested']
 
-y = df['arrested']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.25, random_state=0)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# Bernoulli Naïve Bayes classification
+#model = BernoulliNB()
 
-model = BernoulliNB()
+# Multinomial Naïve Bayes classification
+model = MultinomialNB()
